f760977be468c12527d15ef1b9dc1f1d47f400a8
1kaiyounglee <128564329+1kaiyounglee@users.noreply.github.com>
2026-04-03 09:21:23 +1100

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:  
+        match = defaultdict(list)
+        for word in strs:
+            key_list = [0] * 26
+            for c in sorted(word):
+                key_list[ord(c) - ord('a')] += 1
+            
+            match[tuple(key_list)].append(word)
+
+
+
+        return list(match.values())
